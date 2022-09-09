20e1127247387a444bb931795cf52b85d9782f46
pecezon <96464920+pecezon@users.noreply.github.com>
2022-09-08 18:08:20 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let index = 0
-function changeColors() {
-    var colors = ["red", "blue", "orange", "yellow", "purple", "green"]
-    document.getElementsByTagName("body")[0].style.background = colors[index++]
-    if (index > colors.length - 1) {
-        index = 0
-    }
-}
